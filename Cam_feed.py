10151586c8411ce3d33d3858e5c7051eb6e08ec7
Aidan Pantoya <111801426+aidan-pantoya@users.noreply.github.com>
2023-10-07 16:03:56 -0500

    Update Cam_feed.py

--- Cam_feed.py
+++ Cam_feed.py
@@ -1,41 +1,44 @@
-import pyrealsense2 as rs
 import numpy as np
 import cv2
+import pyrealsense2 as rs
 
-# Configure the depth and color streams of the camera
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+def main():
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start the configured pipeline
-pipeline.start(config)
+    # Start streaming
+    pipeline.start(config)
 
-try:
-    while True:
-        # Get frames from the camera
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+    try:
+        while True:
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        
-        # Map the depth data to a color scheme for visualization
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-        # Stack depth and color images horizontally
-        images = np.hstack((color_image, depth_colormap))
+            # Apply colormap on depth image
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-        # Display the images
-        cv2.imshow('RealSense', images)
-        
-        # Press 'q' on the keyboard to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Stack both images horizontally
+            images = np.hstack((color_image, depth_colormap))
 
-finally:
-    # Stop the pipeline
-    pipeline.stop()
-    cv2.destroyAllWindows()
+            # Show images
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            cv2.waitKey(1)
+
+    finally:
+        # Stop streaming
+        pipeline.stop()
+
+if __name__ == "__main__":
+    main()
